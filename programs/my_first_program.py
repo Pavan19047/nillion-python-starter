2373a91da48ed9097b4643dedd480626c7372712
Pavan K <118459281+Pavan19047@users.noreply.github.com>
2024-06-30 21:18:41 +0530

    Update my_first_program.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,17 @@
 from nada_dsl import *
 
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # Compute the product of my_int1 and my_int2
-    result = my_int1 * my_int2
+    # Compute the GCD of my_int1 and my_int2 using the Euclidean algorithm
+    result = gcd(my_int1, my_int2)
 
     # Return the output with the computed result
-    return [Output(result, "product_output", party1)]
+    return [Output(result, "gcd_output", party1)]
